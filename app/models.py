faad1580bcd2f76bac9392b7458c9693f7e60c4e
Andrey Zhilyakov <andrey@Andreys-MacBook-Air.local>
2018-07-29 07:14:00 -0400

    git check

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(120))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-
+#git check
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
